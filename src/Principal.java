8b83709e6b8f47c40f7c197abd86e945e49095db
Gibellino <bbgibellino@gmail.com>
2016-10-26 11:33:31 +0100

    Calculo Balanço

--- src/Principal.java
+++ src/Principal.java
@@ -10,6 +10,37 @@ public class Principal {
 
 		System.out.println("Olá, Turma.");
 		
+		int quotas = 300,
+			recBaile = 200,
+			recBar = 250;
+						
+		int receitas = quotas + recBaile + recBar;
+		
+		int agua = 80,
+			luz = 60,
+			tlm = 70;
+		
+		int despesas = agua + luz + tlm;
+		
+		System.out.println("\nReceita : \n\n "
+				+ " Quotas: " + quotas 
+				+ " €\n  Baile: " + recBaile 
+				+ " €\n  Bar: " + recBar 
+				+ " €\n\n  Total Receitas: " + receitas
+				+ " €;\n\nDespesas : \n\n"
+				+ "  agua: " + agua
+				+ " €\n  luz:" + luz
+				+ " €\n  telemovel:" + tlm
+				+ " €\n\n  Total Despesas: " + despesas 
+				+ " €\n\nBalanço: " + (receitas-despesas)
+				+ " €");
+		
+		if((receitas-despesas) < 0){
+			System.out.println("\n\nBalanço negativo");
+		}
+		else{
+			System.out.println("\n\nBalanço positivo");
+		}
 	}
 
 }
