22a444fbe1e197fc6e5a1d862bfb84a429a1a735
Gibellino <bbgibellino@gmail.com>
2016-11-02 10:35:34 +0000

    Calculadora Infinita

--- src/dia021116/CalculadoraInfinita.java
+++ src/dia021116/CalculadoraInfinita.java
@@ -12,57 +12,63 @@ public class CalculadoraInfinita {
 		
 		int op = 0;
 		
-		System.out.println("\nIntroduza um número: ");
-		num1 = input.nextInt();
-		
-		System.out.println("\nIntroduza outro número (Se for igual a 0, sai): ");
-		num2 = input.nextInt();
-		
-		System.out.println("\n\tMenu:\n "
-				+ "1 - Soma;\n "
-				+ "2 - Subtração;\n "
-				+ "3 - Multiplicação;\n "
-				+ "4 - Divisão;\n "
-				+ "0 - Sair\n\n");
-		
-		if(num2 != 0){
-			System.out.println("Introduza a opção que deseja! ");
-			op = input.nextInt();
-		
-			switch(op){
+		do{
+			System.out.println("\nIntroduza um número: ");
+			num1 = input.nextInt();
+			
+			System.out.println("\nIntroduza outro número (Se for igual a 0, sai): ");
+			num2 = input.nextInt();
+			
+			if(num2 != 0){
 				
-			case 1: soma(num1,num2); break;
+				System.out.println("\n\tMenu:\n "
+						+ "1 - Soma;\n "
+						+ "2 - Subtração;\n "
+						+ "3 - Multiplicação;\n "
+						+ "4 - Divisão;\n "
+						+ "0 - Sair\n\n");
 			
-			case 2: menos(num1,num2); break;
+				System.out.println("Introduza a opção que deseja! ");
+				op = input.nextInt();
 			
-			case 3: multi(num1,num2); break;
+				System.out.println("\n");
+				
+				switch(op){
+					
+				case 1: soma(num1,num2); break;
+				
+				case 2: menos(num1,num2); break;
+				
+				case 3: multi(num1,num2); break;
+				
+				case 4: divi(num1,num2); break;
+				
+				case 0: System.out.println("\nVai Sair!!\n"); break;
+				
+				default: System.out.println("\nOpção Inválida!!\n");
 			
-			case 4: divi(num1,num2); break;
-			
-			case 0: System.out.println("\nVai Sair!!\n"); break;
-			
-			default: System.out.println("\nOpção Inválida!!\n");
-		
+				}
+			}else{
+				System.out.println("\nVai Sair!!\n");
 			}
-		}else{
-			System.out.println("\nVai Sair!!\n");
 		}
+		while(num2!=0);
 	
 	}
 
 	public void soma(int num1, int num2){
-		for(int i = 0; i<3; System.out.println((num1+num2)));
+		System.out.println((num1+num2));
 	}
 	
 	public void menos(int num1, int num2){
-		for(int i = 0; i<3; System.out.println((num1-num2)));
+		System.out.println((num1-num2));
 	}
 	
 	public void multi(int num1, int num2){
-		for(int i = 0; i<3; System.out.println((num1*num2)));
+		System.out.println((num1*num2));
 	}
 	
 	public void divi(int num1, int num2){
-		for(int i = 0; i<3; System.out.println((num1/num2)));
+		System.out.println((num1/num2));
 	}
 }
